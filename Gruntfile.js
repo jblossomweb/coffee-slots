104943b788905b7ab0edf91f76c0b9c096087ba9
John Blossom <jblossom.web@gmail.com>
2016-08-02 00:40:19 -0400

    copy sounds on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -380,6 +380,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
+            'sounds/{,*/}*.*',
             'styles/fonts/{,*/}*.*'
           ]
         }, {
